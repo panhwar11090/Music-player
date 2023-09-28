917a7de6bbee7d699f9a0d69e6ed7cca773caa6c
panhwar11090 <11090@kiet.edu.pk>
2023-09-28 14:42:55 +0500

    Update script.js

--- script.js
+++ script.js
@@ -50,9 +50,59 @@ function pauseTrack(){
 
 };
 
+// Function to switch Prev song
+function prevTrack(){
+    // Decrement the value of trackindex by 1 to select the track from the track index array
+    trackIndex--;
+    //Check if selected track index is less than 0
+    if(trackIndex < 0){
+        // Reassign the trackIndex to last track in the trackArray
+        trackIndex  = tracks.length-1;
 
+    };
+    // load the selected track
+    loadTrack(tracks[trackIndex]);
+    //play the selected track
+    playTrack();
+};
 
+// Function to switch Next song
+function nextTrack(){
+    // Decrement the value of trackindex by 1 to select the track from the track index array
+    trackIndex ++;
+    //Check if selected track index is less than 0
+    if(trackIndex > tracks.length-1){
+        // Reassign the trackIndex to last track in the trackArray
+        trackIndex  = 0;
 
+    };
+    // load the selected track
+    loadTrack(tracks[trackIndex]);
+    //play the selected track
+    playTrack();
+};
+
+// function to update progeress bar
+function updateProgress(e){
+    //destructure the total duration and currenttime of the audio
+    const {duration , currentTime} = e.srcElement;
+    //Calculate the percentage of overall audio played currenttime and total duration
+    const progeressPercent = currentTime / duration * 100;
+    //Reassing widht of progress bar using the progressPercentage
+    progressBar.style.width = `${progeressPercent}%`
+}
+
+// Function to set the Progress Bar
+function setProgress(e) {
+    // Get the overall width in px for progress bar container
+    const width = this.clientWidth;
+    // Get the x axis px value for the location of click on the progress bar container
+    const clickLocation = e.offsetX;
+    // Get the total duration of the track
+    const duration = audio.duration;
+    // Reassign the currentTime of audio track by calculating based on above metrics
+    audio.currentTime = clickLocation / width * duration;
+};
 
 // Event listeners
 // listen for play btn
@@ -68,5 +118,18 @@ playBtn.addEventListener('click' , () =>{
     }
 });
 
+//2 Listen for click on prev btn
+previousBtn.addEventListener('click', prevTrack);
 
+//3 Listen for click on next btn
+nextBtn.addEventListener('click', nextTrack);
+
+//4 Listen for a timeupddate on audio element
+audio.addEventListener('timeupdate', updateProgress)
+
+// 5. Listen for click on the progress bar
+progress.addEventListener('click', setProgress);
+
+// 6. Listen for end of playback for current track
+audio.addEventListener('ended', nextTrack);
 
